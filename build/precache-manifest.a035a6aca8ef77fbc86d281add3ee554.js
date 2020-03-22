59f5f3e9a473503143bc164fa6e833071698ec4b
Asto7 <ssatyanand7@gmail.com>
2020-03-22 21:49:48 +0530

    build

---
+++ build/precache-manifest.a035a6aca8ef77fbc86d281add3ee554.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0192bfea8349aa65bab26aa67730a81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7402eadc0edb32e37b7e",
+    "url": "/static/css/2.5b7efe6d.chunk.css"
+  },
+  {
+    "revision": "46d6105079911f00ddbd",
+    "url": "/static/css/main.df09c1a7.chunk.css"
+  },
+  {
+    "revision": "7402eadc0edb32e37b7e",
+    "url": "/static/js/2.8bc42707.chunk.js"
+  },
+  {
+    "revision": "515898009023b0d4309bd6adbe7fb9f1",
+    "url": "/static/js/2.8bc42707.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46d6105079911f00ddbd",
+    "url": "/static/js/main.aebc6c59.chunk.js"
+  },
+  {
+    "revision": "7a008b5c3a6c3fefae51",
+    "url": "/static/js/runtime-main.18445cb3.js"
+  }
+]);
